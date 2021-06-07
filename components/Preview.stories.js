ef41d4e8f2c41a4049fe71633b710c38759ded0a
Neto Ramalho <netto.ramalho@gmail.com>
2021-06-07 19:20:30 -0300

    Updating preview stories images

--- components/Preview.stories.js
+++ components/Preview.stories.js
@@ -2,4 +2,4 @@ export default {
   title: 'Preview'
 }
 
-export const Default = () => '<Preview :images="[\'conecta/desktop-distribution.png\', \'conecta/mobile-distribution.png\']" />'
+export const Default = () => '<Preview :images="[\'conecta/desktop-distribution.webp\', \'conecta/mobile-distribution.webp\']" />'
--- components/Project.stories.js
+++ components/Project.stories.js
@@ -2,6 +2,6 @@ export default {
   title: 'Project'
 }
 
-export const Default = args => '<Project name="Project name" description="A great description of this project" front="Language, Framework, E2E" back="Language, Framework, Database" :images="[\'conecta/desktop-distribution.png\']"/>'
+export const Default = args => '<Project name="Project name" description="A great description of this project" front="Language, Framework, E2E" back="Language, Framework, Database" :images="[\'conecta/desktop-distribution.webp\']"/>'
 
-export const Reversed = args => '<Project :reverse="true" name="Project name" description="A great description of this project" front="Language, Framework, E2E" back="Language, Framework, Database" :images="[\'conecta/desktop-distribution.png\']"/>'
+export const Reversed = args => '<Project :reverse="true" name="Project name" description="A great description of this project" front="Language, Framework, E2E" back="Language, Framework, Database" :images="[\'conecta/desktop-distribution.webp\']"/>'
